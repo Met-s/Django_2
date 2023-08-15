5ad63c003db97e176e61c11ca00249f03778a3ac
Met-s <den_vo@mail.ru>
2023-08-15 14:33:08 +0300

    Изменил переменные

--- news_portal/news/models.py
+++ news_portal/news/models.py
@@ -1,22 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import Sum
+# ---------------------------------------------------
 
 
 class Author(models.Model):
-    authorUser = models.OneToOneField(User, on_delete=models.CASCADE)
-    ratingAuthor = models.SmallIntegerField(default=0)
+    author_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating_author = models.SmallIntegerField(default=0)
 
-    def update_rating(self):
-        postRat = self.post_set.aggregate(postRating=Sum('rating'))
-        pRat = 0
-        pRat += postRat.get('postRating')
+    def update_rating(self, prat, crat):
+        post_rat = self.post_set.aggregate(postrating=Sum('rating'))
 
-        commentRat = self.authorUser.comment_set.aggregate(
-            commentRating=Sum('rating'))
-        cRat = 0
-        cRat += commentRat.get('commentRating')
-        self.ratingAuthor = pRat * 3 + cRat
+        prat += post_rat.get('postrating')
+
+        comment_rat = self.author_user.comment_set.aggregate(
+            commentrating=Sum('rating'))
+
+        crat += comment_rat.get('commentrating')
+        self.rating_author = prat * 3 + crat
         self.save()
 
 
@@ -32,9 +33,9 @@ class Post(models.Model):
         (NEWS, 'Новость'),
         (ARTICLE, 'Статья'),
     )
-    categoryType = models.CharField(max_length=2, choices=CATEGORY_CHOICES)
-    dateCreation = models.DateTimeField(auto_now_add=True)
-    postCategory = models.ManyToManyField(Category, through='PostCategory')
+    category_type = models.CharField(max_length=2, choices=CATEGORY_CHOICES)
+    date_creation = models.DateTimeField(auto_now_add=True)
+    post_category = models.ManyToManyField(Category, through='PostCategory')
     title = models.CharField(max_length=54)
     text = models.TextField()
     rating = models.SmallIntegerField(default=0)
@@ -52,15 +53,15 @@ class Post(models.Model):
 
 
 class PostCategory(models.Model):
-    postThrough = models.ForeignKey(Post, on_delete=models.CASCADE)
-    categoryThrough = models.ForeignKey(Category, on_delete=models.CASCADE)
+    post_through = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category_through = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
 class Comment(models.Model):
-    commentPost = models.ForeignKey(Post, on_delete=models.CASCADE)
-    commentUser = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment_user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
-    dateCreation = models.DateTimeField(auto_now_add=True)
+    date_creation = models.DateTimeField(auto_now_add=True)
     rating = models.SmallIntegerField(default=0)
 
     def like(self):
