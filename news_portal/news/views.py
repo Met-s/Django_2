cecab35d44db01649031dc3219d1fb77f2c92554
Met-s <den_vo@mail.ru>
2023-08-22 18:26:13 +0300

    Пагинация

--- news_portal/news/views.py
+++ news_portal/news/views.py
@@ -8,6 +8,7 @@ class PostList(ListView):
     ordering = '-date_creation'
     template_name = 'post_list.html'
     context_object_name = 'news'
+    paginate_by = 10
 
 
 class PostDetailView(DetailView):
